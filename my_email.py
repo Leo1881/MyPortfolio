cd68bc7437265c58d910684da2163976fa36c7de
Lee <leaveil.johnson1881@gmail.com>
2024-08-26 15:25:15 +0200

    Send email using form

--- my_email.py
+++ my_email.py
@@ -2,18 +2,17 @@ import smtplib
 import ssl
 import certifi
 
-host = "smtp.gmail.com"
-port = 465
-username = "leaveil.johnson1881@gmail.com"
-password = "wyzh gefj hgee oxqc"
 
-receiver = "leaveil.johnson1881@gmail.com"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "leaveil.johnson1881@gmail.com"
+    password = "wyzh gefj hgee oxqc"
 
-context = ssl.create_default_context(cafile=certifi.where())
+    receiver = "leaveil.johnson1881@gmail.com"
 
-message = """\
-Subject: Hi! 
-How are you?"""
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    context = ssl.create_default_context(cafile=certifi.where())
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
