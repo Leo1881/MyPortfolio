16c253400636c45ce74f3a53496dd217b19d5cb9
Lee <leaveil.johnson1881@gmail.com>
2024-08-26 15:11:06 +0200

    Send email

---
+++ my_email.py
@@ -0,0 +1,19 @@
+import smtplib
+import ssl
+import certifi
+
+host = "smtp.gmail.com"
+port = 465
+username = "leaveil.johnson1881@gmail.com"
+password = "wyzh gefj hgee oxqc"
+
+receiver = "leaveil.johnson1881@gmail.com"
+
+context = ssl.create_default_context(cafile=certifi.where())
+
+message = """\
+Subject: Hi! 
+How are you?"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username, receiver, message)
