f318d592a1aed08350603afb24d171c1a3636211
Lee <leaveil.johnson1881@gmail.com>
2024-08-26 13:15:28 +0200

    Add description, images and links

--- main.py
+++ main.py
@@ -19,14 +19,20 @@ with col2:
 content2 = """Below is a list of all my projects built using python. Feel free to contact me"""
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"], width=300)
+        st.write(f"[Source Code]({row['url']})")
