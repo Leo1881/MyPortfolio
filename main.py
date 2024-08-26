c698a216eb0028ad51d1d877e056dfeeedbcf535
Lee <leaveil.johnson1881@gmail.com>
2024-08-26 12:23:30 +0200

    Add columns and text detail

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/Lee.jpg")
+
+with col2:
+    st.title("Lee Johnson")
+    content = """I'm Lee, a passionate graphic designer, Python programmer, and web developer with a keen eye for aesthetics and a 
+    strong command of code. I blend creativity with technical expertise to craft visually stunning designs, build 
+    dynamic websites, and develop efficient Python solutions. Dedicated to delivering high-quality results that meet 
+    client needs and exceed expectations."""
+    st.info(content)
